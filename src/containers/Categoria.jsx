ad463da5a2bedbc68b661aab134c498ec3e8e744
Gnieva94 <gus_94@hotmail.com.ar>
2024-01-17 02:50:27 -0300

    Estilo en Menu y Categoria.

--- src/containers/Categoria.jsx
+++ src/containers/Categoria.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useContext } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import Contexto from '../context/Contexto'
+import '../assets/css/Menu/menu.css'
 
 export const Categoria = () => {
   const { subcategory, getSubcategory } = useContext(Contexto)
@@ -11,25 +12,29 @@ export const Categoria = () => {
   }, [])
 
   return (
-    <main>
-      <div>
-        <span><Link to={'/menu'}>Menú</Link> / {subcategory.title}</span>
-        <h2>{subcategory.title}</h2>
-      </div>
-      <section>
+    <main className='menu-main'>
+      <section className='menu-section'>
+        <div className='menu-headings'>
+          <span className='menu-heading-span'><Link to={'/menu'}>Menú /</Link> {subcategory.title}</span>
+          <h2>{subcategory.title}</h2>
+        </div>
         {
           (subcategory && subcategory.subcategory) && 
           subcategory.subcategory.map((subcat)=>(
-            <article key={subcat.id}>
-              <h3>{subcat.title}</h3>
+            <article className='menu-article' key={subcat.id}>
+              <h3 className='menu-article-heading'>{subcat.title}</h3>
+              <ul className='menu-article-ul'>
               {subcat.products.map((prod)=>(
-                <li key={prod.id}>
-                <Link to={`/producto/${prod.id}`}>
-                  <img src={prod.imgSrc} alt={prod.imgAlt} />
-                  <h4>{prod.name}</h4>
+                <li className='menu-article-ul-li' key={prod.id}>
+                <Link className='menu-article-link' to={`/producto/${prod.id}`}>
+                  <div className='img-circle'>
+                    <img className='menu-article-img' src={prod.imgSrc} alt={prod.imgAlt} />
+                  </div>
+                  <h4 className='menu-article-name'>{prod.name}</h4>
                 </Link>
               </li>
               ))}
+              </ul>
             </article>
           ))
         }
--- src/routes/Menu.jsx
+++ src/routes/Menu.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useContext } from 'react'
 import { Link } from 'react-router-dom'
 import Contexto from '../context/Contexto'
+import '../assets/css/Menu/menu.css'
 
 export const Menu = () => {
   const { menu, getMenu } = useContext(Contexto)
@@ -8,23 +9,26 @@ export const Menu = () => {
     getMenu()
   }, [])
   return (
-    <main>
-      <div>
-        <h3>¡Disfrútalos!</h3>
-        <h2>Conoce nuestras bebidas y alimentos de temporada</h2>
-      </div>
-      <section>
+    <main className='menu-main'>
+      <section className='menu-section'>
+        <div className='menu-headings'>
+          <h3>¡Disfrútalos!</h3>
+          <h2>Conoce nuestras bebidas y alimentos de temporada</h2>
+        </div>
         {menu &&
           menu.map((item) => (
-            <article key={item.id}>
-              <h3>{item.title}</h3>
-              <ul>
+            <article className='menu-article' key={item.id}>
+              <h3 className='menu-article-heading'>{item.title}</h3>
+              <ul className='menu-article-ul'>
                 {
                   item.category.map((cat) => (
-                    <li key={cat.id}>
-                      <Link to={`./${item.paramMenu}/${cat.param}`}>
-                        <img src={cat.img} alt={cat.title} />
-                        <h4>{cat.title}</h4>
+                    <li className='menu-article-ul-li' key={cat.id}>
+                      <Link className='menu-article-link' to={`./${item.paramMenu}/${cat.param}`}>
+                        <div className='img-circle'>
+                        <img className='menu-article-img' src={cat.img} alt={cat.title} />
+
+                        </div>
+                        <h4 className='menu-article-name'>{cat.title}</h4>
                       </Link>
                     </li>
                   ))
